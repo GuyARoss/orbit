87e55a45c63d5c91437be174b2b5bfc61c2ab43f
Guy Ross <guyallenross@gmail.com>
2021-10-31 03:53:51 -0400

    resolved broken tests

--- pkg/fs/fs_test.go
+++ pkg/fs/fs_test.go
@@ -6,37 +6,28 @@ import (
 )
 
 func TestCondenseFilePath_LongPath(t *testing.T) {
-	path := filepath.Clean(".orbit\\base\\web\\pages\\home.jsx")
+	path := filepath.Clean(".orbit/base/web/pages/home.jsx")
 
 	got := condenseFilePath(path)
-	if got != ".orbit\\base\\pages\\home.jsx" {
-		t.Errorf("expected: %s, got %s", ".orbit\\pages\\home.jsx", got)
-	}
-}
-
-func TestCondenseFilePath_ShortPath(t *testing.T) {
-	path := filepath.Clean(".orbit\\base\\home.jsx")
-
-	got := condenseFilePath(path)
-	if got != ".orbit\\base\\home.jsx" {
-		t.Errorf("expected: %s, got %s", ".orbit\\pages\\home.jsx", got)
+	if got != ".orbit/base/pages/home.jsx" {
+		t.Errorf("expected: %s, got %s", ".orbit/pages/home.jsx", got)
 	}
 }
 
 func TestCondenseDirPath_LongDir(t *testing.T) {
-	path := filepath.Clean(".orbit\\base\\web\\pages")
+	path := filepath.Clean(".orbit/base/web/pages")
 
 	got := condenseDirPath(path)
-	if got != ".orbit\\base\\pages" {
-		t.Errorf("expected: %s, got %s", ".orbit\\base\\pages", got)
+	if got != ".orbit/base/pages" {
+		t.Errorf("expected: %s, got %s", ".orbit/base/pages", got)
 	}
 }
 
 func TestCondenseDirPath_ShortDir(t *testing.T) {
-	path := filepath.Clean(".orbit\\base\\pages")
+	path := filepath.Clean(".orbit/base/pages")
 
 	got := condenseDirPath(path)
-	if got != ".orbit\\base\\pages" {
-		t.Errorf("expected: %s, got %s", ".orbit\\base\\pages", got)
+	if got != ".orbit/base/pages" {
+		t.Errorf("expected: %s, got %s", ".orbit/base/pages", got)
 	}
 }
