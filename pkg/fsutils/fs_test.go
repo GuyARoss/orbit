a917e248c0ee7a7a8794a80fb5b040f741a39051
Guy Ross <guyallenross@gmail.com>
2022-02-28 15:57:10 -0500

    resolve windows related issues

--- pkg/fsutils/fs_test.go
+++ pkg/fsutils/fs_test.go
@@ -1,33 +1,32 @@
 package fsutils
 
 import (
-	"path/filepath"
 	"testing"
 )
 
 func TestCondenseFilePath_LongPath(t *testing.T) {
-	path := filepath.Clean(NormalizePath(".orbit/base/web/pages/home.jsx"))
+	path := ".orbit/base/web/pages/home.jsx"
 
 	got := condenseFilePath(path)
-	if got != NormalizePath(".orbit/base/pages/home.jsx") {
-		t.Errorf("expected: %s, got %s", NormalizePath(".orbit/pages/home.jsx"), got)
+	if got != ".orbit/base/pages/home.jsx" {
+		t.Errorf("expected: %s, got %s", ".orbit/pages/home.jsx", got)
 	}
 }
 
 func TestCondenseDirPath_LongDir(t *testing.T) {
-	path := filepath.Clean(NormalizePath(".orbit/base/web/pages"))
+	path := ".orbit/base/web/pages"
 
 	got := condenseDirPath(path)
-	if got != NormalizePath(".orbit/base/pages") {
-		t.Errorf("expected: %s, got %s", NormalizePath(".orbit/base/pages"), got)
+	if got != ".orbit/base/pages" {
+		t.Errorf("expected: %s, got %s", ".orbit/base/pages", got)
 	}
 }
 
 func TestCondenseDirPath_ShortDir(t *testing.T) {
-	path := filepath.Clean(NormalizePath(".orbit/base/pages"))
+	path := ".orbit/base/pages"
 
 	got := condenseDirPath(path)
-	if got != NormalizePath(".orbit/base/pages") {
-		t.Errorf("expected: %s, got %s", NormalizePath(".orbit/base/pages"), got)
+	if got != ".orbit/base/pages" {
+		t.Errorf("expected: %s, got %s", ".orbit/base/pages", got)
 	}
 }
