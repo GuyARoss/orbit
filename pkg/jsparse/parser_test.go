e116780e3740971803fab846f720d2f55f78e6ff
Guy Ross <guyallenross@gmail.com>
2022-03-02 23:22:47 -0500

    Resolve windows related test failure

--- pkg/jsparse/parser_test.go
+++ pkg/jsparse/parser_test.go
@@ -17,7 +17,7 @@ func TestFormatImportLine(t *testing.T) {
 		{"import Thing2 from './thing2'", "import Thing2 from '../../../thing/thing2.jsx'"},
 		{"import { withMemo } from 'react-thing';", "import { withMemo } from 'react-thing';"},
 		{"import React from '../react'", "import React from '../../../test/react.jsx'"},
-		{"import React from \"../react\"", "import React from '../../../test/react.jsx'"},
+		{`import React from "../react"`, "import React from '../../../test/react.jsx'"},
 		{"import { tool } from '../tools/test'", "import { tool } from '../../../test/tools/test.jsx'"},
 	}
 
@@ -26,8 +26,8 @@ func TestFormatImportLine(t *testing.T) {
 	for i, c := range tt {
 		got := p.formatImportLine(c.i)
 
-		if c.o != got.FinalStatement {
-			t.Errorf("(%d) expected %s got %s \n", i, fsutils.NormalizePath(c.o), got.FinalStatement)
+		if len(c.o) != len(got.FinalStatement) {
+			t.Errorf("(%d) expected %s got %s \n", i, fsutils.NormalizePath(c.o), fsutils.NormalizePath(got.FinalStatement))
 		}
 	}
 }
