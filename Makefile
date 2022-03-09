ca8758d95578ec1f25242a013c7d120b1c44411d
Guy Ross <guyallenross@gmail.com>
2022-03-08 23:19:29 -0500

    added licensing

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ build:
 example:	
 	make build
 	./scripts/link_examples.sh
+
+license:
+	python3 ./scripts/license.py
--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2021 Guy A. Ross
+// This source code is licensed under the GNU GPLv3 found in the
+// LICENSE file in the root directory of this source tree.
 package main
 
 import "github.com/GuyARoss/orbit/cmd"
