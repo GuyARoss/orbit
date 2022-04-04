9e26cea681ebdd54ec24d203a359377fc70a4cd4
Guy Ross <guyallenross@gmail.com>
2022-04-03 20:32:39 -0400

    update license

--- pkg/webwrap/react.go
+++ pkg/webwrap/react.go
@@ -28,8 +28,6 @@ type ReactWebWrapper struct {
 	elementID string
 }
 
-// @@todo: javascript should run document.onload
-
 var ErrComponentExport = errors.New("prefer capitalization for jsx components")
 var ErrInvalidComponent = errors.New("invalid jsx component")
 
--- pkg/webwrap/react_ssr.go
+++ pkg/webwrap/react_ssr.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2021 Guy A. Ross
+// This source code is licensed under the GNU GPLv3 found in the
+// license file in the root directory of this source tree.
+
 package webwrap
 
 import (
