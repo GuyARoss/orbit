d65e137128e510aa7415922f881989f38e8c58af
Guy Ross <guyallenross@gmail.com>
2022-05-08 14:50:56 -0400

    fin

--- scripts/build-times.py
+++ scripts/build-times.py
@@ -1,19 +1,19 @@
+'''
+This script measures the time to build over time for orbit's build command.
+'''
 import os
 import time
 import tempfile
 import subprocess
-from datetime import datetime
 from typing import List
-from requests.auth import HTTPBasicAuth
-'''
-This script measures the time to build over time for orbit's build command.
-'''
+import matplotlib.pyplot as plt
 
-def main() -> None:
-    tags = ['v0.7.1', 'v0.7.2', 'v0.3.6', 'v0.3.2', 'v0.3.0', 'v0.2.0']
 
+def main(tags: List[str]) -> None:
+    init_dir = os.getcwd()
     tag_stats = []
-    for tag in tags[0:1]:
+    
+    for tag in tags:
         tmpdir = tempfile.mkdtemp()
         print('new tempdir', tmpdir)
         os.chdir(tmpdir)            
@@ -22,18 +22,32 @@ def main() -> None:
         print(os.getcwd())
         stats = profile_build_cmd(tag)
         tag_stats.append(stats)
-        
-    print(tag_stats)
-    # TODO: sort tag stats
-    # 5 - display in matplotlib    
+    
+    os.chdir(init_dir)    
+    plot_from_stats('./build-times.png', tag_stats)
 
 class ProfilerStats:
     tag: str
     runtime_duration: int
 
     def __init__(self, tag: str, duration: int) -> None:
-        self.version_tag = tag
+        self.tag = tag
         self.runtime_duration = duration        
+
+def plot_from_stats(path: str, stats: List[ProfilerStats]):
+    points = []
+    lbls = []
+
+    for s in stats:
+        points.append(s.runtime_duration)
+        lbls.append(s.tag)
+
+    plt.xticks(range(len(points)), [lbl.replace('commit@', '')[:6] for lbl in lbls])
+    plt.ylabel('Duration')
+    plt.xlabel('Version No.')
+    plt.title('Runtime duration (orbit build cmd)')
+    plt.bar(range(len(points)), points) 
+    plt.savefig(path)
 
 def profile_build_cmd(tag: str) -> ProfilerStats:
     try:
@@ -41,32 +55,60 @@ def profile_build_cmd(tag: str) -> ProfilerStats:
     except:
         raise Exception('npm install failed')
     
-    start_time = time.time()    
+    sum_of_times = 0
+    for _ in range(5):
+        start_time = time.time()    
 
-    try:
-        subprocess.check_output([f'./orbit build --pacname=orbitgen'], shell=True)
-    except:
-        raise Exception('build command failed')
+        try:
+            subprocess.check_output([f'./orbit build --pacname=orbitgen'], shell=True)
+        except:
+            raise Exception('build command failed')
 
-    end_time = time.time()
+        end_time = time.time()
+        sum_of_times += end_time - start_time
 
-    return ProfilerStats(tag, end_time - start_time)
+    return ProfilerStats(tag, sum_of_times / 5)
 
 def setup_repo(dir: str, tag: str) -> str:
-    try:
-        subprocess.check_output([f'git clone --branch {tag} http://github.com/GuyARoss/orbit'], shell=True)
+    try:        
+        if tag == 'latest':
+            subprocess.check_output([f'git clone http://github.com/GuyARoss/orbit'], shell=True)
+            os.chdir(dir + "/orbit")
+        elif "commit@" in tag:
+            commit = tag.replace('commit@', '')
+
+            subprocess.check_output(['git clone http://github.com/GuyARoss/orbit'], shell=True)
+            os.chdir(dir + "/orbit")
+            subprocess.check_output([f'git checkout {commit}'], shell=True)
+        else:
+            subprocess.check_output([f'git clone --branch {tag} http://github.com/GuyARoss/orbit'], shell=True)
+            os.chdir(dir + "/orbit")
+
     except:
+        print('AT CURRENT DIR', dir)
         raise Exception('github clone failed')
 
-    try:
+    try:        
         subprocess.check_output([f'go build -o ./orbit'], shell=True)
+        subprocess.check_output([f'./scripts/link_examples.sh'], shell=True)
     except:
         raise Exception('make example failed')
 
     return dir + "/orbit/examples/basic-react"
 
-def create_working_dir():    
-    pass
-
 if __name__ == "__main__":
-    main()
+    # tags = ['v0.2.0', 'v0.3.0', 'v0.3.2', 'v0.3.6', 'v0.7.0', 'v0.7.1', 'latest']
+
+    tags = [
+        'latest',
+        'commit@6a131ded2e281846a1ca71d87a41ee14c30bcdfa',
+        'commit@9184fce235309eb26a0451d4facbecd1aa3566bb',
+        'commit@e5f0835c519cf400c2fd4bbd41f8e5a30fb1b09a',
+        'commit@4cbfbbb0b4deaf770274b65591bd310f418791d9',
+        'commit@571d3ced6cec838c622a47201be5420d3ff0ee16',
+        'commit@1cbf8146637b88c37e8e93338051325ea2077f00',
+        'commit@4e52e3ce9cd5b0689d00d395caa182afe983debd']
+    tags.reverse()
+    main(
+        tags
+    )
